2045185886b3ffbdcf9d31578a62b6fcbade3883
Savitar38 <auotutu@utanotacom>
2022-02-21 23:31:01 +0100

    Adding save and changing icons

--- paint.py
+++ paint.py
@@ -1,5 +1,8 @@
 import tkinter as tk
 from tkinter import colorchooser
+import random
+from PIL import Image
+from tkinter.filedialog import asksaveasfile
 
 window = tk.Tk()
 
@@ -7,20 +10,45 @@ x = 0
 y = 0
 
 prepinac = ""
+pred_prepinac = ""
 farba = "#000"
 
+# PRÍPRAVA PRE ZOBRAZENIE IKON, VYTVORÍME SI DVA LISTY -- JEDEN SO SVETLÝMI A DRUHÝ S TMAVÝMI (STLAČENÝMI) IKONAMY
 ikony = ["sprej.png", "stvorec.png", "kruh.png", "farba.png", "ciara.png", "stetec.png", "vymazat.png", "ulozit.png"]
 tlacidla = ["sprej", "stvorec", "kruh", "farba", "ciara", "stetec", "vymazat", "ulozit"]
+ikony.sort()
+tlacidla.sort()
 
-plocha = tk.Canvas(width=440, height=400, bg="red")
+ikony_subory = []
+ikony_tmave = []
+for i in range(8):
+    img = tk.PhotoImage(file=f"./icons/{ikony[i]}")
+    ikony_subory.append(img)
+
+for i in range(8):
+    img = tk.PhotoImage(file=f"./icons_dark/{ikony[i]}")
+    ikony_tmave.append(img)
+
+
+# VYTVORÍME CANVAS KDE BUDEME KRESLIŤ
+plocha = tk.Canvas(width=440, height=400, bg="white")
 plocha.grid(row=1, column=0, columnspan=8)
 
+# JEDNODUCHÁ FUNKCIA, KTORÁ ZISTÍ, AKÁ IKONA BOLA STLAČENÁ A PODÁ TÚTO INFORMÁCIU ĎALŠEJ FUNKCII 
+def zmen_ikonu(prepinac):
+    ikona = f"{prepinac}.png"
+    index = ikony.index(ikona)
+    zobrazit_tlacidla(index)
+
+# HLAVNÁ FUNKCIA NA PREPÍNANIE FUNKCIÍ
 def prepnut(volba):
 
     ## Voľba prepínača aby sme mohli vybrať správnu funkcionalitu ##
-    print(volba)
     global prepinac
+    global pred_prepinac
+    pred_prepinac = prepinac
     if prepinac == volba:
+        zobrazit_tlacidla()
         prepinac = ""
         return
     else:
@@ -30,40 +58,44 @@ def prepnut(volba):
 
     if prepinac == "farba":
         global farba
-        farba = colorchooser.askcolor(title ="Vyber farbu") # Output je v hexadecimálnej sústave
-        prepinac = ""
+        pred_farba = farba
+        rgb, farba = colorchooser.askcolor(title ="Vyber farbu") # Output je v hexadecimálnej sústave
+        if farba == None:
+            farba = pred_farba
+        prepinac = pred_prepinac
 
     elif prepinac == "vymazat":
         plocha.delete("all")
-        prepinac = ""
+        prepinac = pred_prepinac
 
     elif prepinac == "ulozit":
+        plocha.postscript(file="kresba.eps")
+        img = Image.open("kresba.eps")
+        file = asksaveasfile(defaultextension = ".png")
+        img.save(file.name)
         prepinac = ""
+        zobrazit_tlacidla()
+
+    # O ostatné funkcionality, ktoré sú dynamické, sa postaráme nižšie, tu len zmeníme ikonu na aktívnu
+    else:
+        zmen_ikonu(prepinac)
 
 
 
-## VYTVÁRANIE TLAČIDIEL ##
-ikony_subory = []
-for i in range(8):
-    img = tk.PhotoImage(file=f"./icons/{ikony[i]}")
-    ikony_subory.append(img)
+## VYTVÁRANIE A RENDEROVANIE TLAČIDIEL, AK JE INPUT NEŠPECIFIKOVANÝ, VŠETKY IKONY BUDÚ BIELE##
+def zobrazit_tlacidla(volba=10):
+    for i in range(8):
+        if i == volba:
+            b = tk.Button(image=ikony_tmave[i], width=50, command= lambda i=i: prepnut(tlacidla[i]))
+            b.grid(row=0, column=i)
+        else:
+            b = tk.Button(image=ikony_subory[i], width=50, command= lambda i=i: prepnut(tlacidla[i]))
+            b.grid(row=0, column=i)
 
+# PRVOTNÉ RENDEROVANIE TLAČIDIEL
+zobrazit_tlacidla()
 
-for i in range(8):
-    b = tk.Button(image=ikony_subory[i], width=50, command= lambda i=i: prepnut(tlacidla[i]))
-    b.grid(row=0, column=i)
-
-
-
-## SEM VKLADAJTE VAŠE FUNKCIE ##
-
-
-
-
-
-
-## -------------------------- ##
-
+# PO KLIKNUTÍ ZAPÍŠEME SÚRADNICE PÔVODNÉHO KLIKU
 def klik(e):
 
     ## Do globálnych premenných x a y sa uložia súradnice miesta kliku ##
@@ -72,6 +104,7 @@ def klik(e):
     x=e.x 
     y=e.y
 
+# DYNAMICKY REAGUJEME NA SÚRADNICE MYŠI A PRIEBEŽNE RENDERUJEME TVAR PODĽA VÝBERU PREPÍNAČA
 def hyb(e):
     global x,y
     global farba
@@ -79,21 +112,44 @@ def hyb(e):
     ## Do každej funkcie, ktorá niečo kreslí, máte 3 vstupy: originálne súradnice kliku, súradnice aktuálnej pozície stlačenej myše a farbu ##
 
     if prepinac == "sprej":
-        sprej(x, y, e.x, e.y, farba)
+        for i in range(100):
+            sx = random.randint(-13, 13)
+            sy = random.randint(-13, 13)
+            plocha.create_line(e.x+sx, e.y+sy, e.x+sx+1, e.y+sy, fill=farba)
 
     elif prepinac == "stvorec":
-        stvorec(x, y, e.x, e.y, farba)
+        plocha.delete("stvorec")
+        plocha.create_rectangle(x, y, e.x, e.y, fill = farba, outline = farba, tag = "stvorec")
 
     elif prepinac == "kruh":
-        kruh(x, y, e.x, e.y, farba)
+        plocha.delete("kruh")
+        plocha.create_oval(x, y, e.x, e.y, fill = farba, outline = farba, tag = "kruh")
 
     elif prepinac == "ciara":
-        ciara(x, y, e.x, e.y, farba)
+        plocha.delete("ciara")
+        plocha.create_line(x, y, e.x, e.y, fill = farba, tag = "ciara", width=1)
 
     elif prepinac == "stetec":
-        stetec(x, y, e.x, e.y, farba)
+        plocha.create_oval(e.x-10, e.y-10, e.x+10, e.y+10, fill = farba, outline = farba)
 
+# GEOMETRICKÉ TVARY RENDERUJEME AŽ PO UVOLNENÍ TLAČIDLA MYŠI, ČO SI VYŽADUJE SAMOSTATNÚ FUNKCIU
+def umiestnit(e):
+    global x,y
+    global farba
+
+    if prepinac == "stvorec":
+        plocha.create_rectangle(x, y, e.x, e.y, fill = farba, outline = farba)
+
+    elif prepinac == "kruh":
+        plocha.create_oval(x, y, e.x, e.y, fill = farba, outline = farba)
+
+    elif prepinac == "ciara":
+        plocha.create_line(x, y, e.x, e.y, fill = farba, width=1)
+
+# AKCIE MYŠI PREPOJÍME S JEDNOTLIVÝMI FUNKCIAMI
 plocha.bind("<Button-1>",klik)
 plocha.bind("<B1-Motion>",hyb)
+plocha.bind("<ButtonRelease-1>", umiestnit)
+
 
 window.mainloop()
